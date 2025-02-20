e7e047b0a09088fc06feb4edc2e273f60ca865b0
Felix Rieseberg <felix@felixrieseberg.com>
2025-02-20 10:54:02 -0800

    Deal with resedit segfaults

---
+++ tools/resedit.js
@@ -0,0 +1,24 @@
+const resedit = require('../node_modules/@electron/packager/dist/resedit.js')
+const package = require('../package.json');
+
+const exePath = process.argv[process.argv.length - 1]
+
+console.log(exePath)
+
+async function main() {
+  
+  await resedit.resedit(exePath, {
+    "productVersion": package.version,
+    "fileVersion": package.version,
+    "productName": package.productName,
+    "win32Metadata": {
+      "FileDescription": package.productName,
+      "InternalName": package.name,
+      "OriginalFilename": `${package.name}.exe`,
+      "ProductName": package.productName,
+      "CompanyName": package.author
+    }
+  });
+}
+
+main();
